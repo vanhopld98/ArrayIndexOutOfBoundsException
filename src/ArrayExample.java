f8cf89c467b7c411d940393b84fd7121139b14d0
vanhopld98 <vanhopld98@gmail.com>
2021-08-24 17:21:14 +0700

    array

---
+++ src/ArrayExample.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random random = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sách phần tử của mảng :");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = random.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của 1 phần tử bất kỳ: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
